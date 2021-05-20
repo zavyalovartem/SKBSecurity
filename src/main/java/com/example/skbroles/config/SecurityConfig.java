522b090f3a5544d4d54ba4012232f40839cb2de7
zavyalovartem <zavyalovartem@users.noreply.github.com>
2021-05-20 23:33:27 +0500

    Removed unnecessary annotations

--- src/main/java/com/example/skbroles/config/SecurityConfig.java
+++ src/main/java/com/example/skbroles/config/SecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-@EnableWebSecurity
 @EnableGlobalMethodSecurity(jsr250Enabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
